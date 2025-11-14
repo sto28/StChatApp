7b0fa0663d6878512da6e3f399a2c47c053afacf
Stephen Onu <stephenonu16@yahoo.com>
2025-11-14 16:27:54 +0000

    Frontend Routes and pages setup, daisy ui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import React from 'react';
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import SettingsPage from './pages/SettingsPage';
 const App = () => {
   return (
-    <div className='text-red-500'>Ello mate</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route pathe="/" element={<HomePage/>}/>
+        <Route pathe="/signup" element={<SignUpPage/>}/>
+        <Route pathe="/login" element={<LoginPage/>}/>
+        <Route pathe="/settings" element={<SettingsPage/>}/>
+        <Route pathe="/profile" element={<ProfilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
